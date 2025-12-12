86bacd789c3fd49063f608abea29208f5d1903b9
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-12-12 10:01:01 -0300

    feat(agent) Connecting to Kafka and publishing a test message.

--- agents/src/main/java/com/overwatch/agents/AgentsApplication.java
+++ agents/src/main/java/com/overwatch/agents/AgentsApplication.java
@@ -1,7 +1,10 @@
 package com.overwatch.agents;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class AgentsApplication {
@@ -9,5 +12,10 @@ public class AgentsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgentsApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner commandLineRunner ( KafkaTemplate<String,String> kafkaTemplate ){
+        return args -> kafkaTemplate.send("overwatch.published-supers","data","Hello World!.");
+    }
 
 }
