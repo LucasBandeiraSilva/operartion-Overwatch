7d4b666add3e37c3f2d259d63f69ab459e76b83a
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-12-16 14:15:45 -0300

    feat(Logistic) Add the url to access the  file in the bucket.

--- logistic/src/main/java/com/overwatch/logistic/interfaces/BucketController.java
+++ logistic/src/main/java/com/overwatch/logistic/interfaces/BucketController.java
@@ -5,10 +5,7 @@ import com.overwatch.logistic.infrastructure.storage.minio.service.BucketService
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -30,6 +27,16 @@ public class BucketController {
         }
         catch (Exception e){
             return ResponseEntity.status(500).body("Error when sending the archive " + e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<String>getUrl(@RequestParam String filename){
+        try {
+            String url = service.getUrl(filename);
+            return ResponseEntity.ok(url);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error when retrieving file URL: " + e.getMessage());
         }
     }
 }
