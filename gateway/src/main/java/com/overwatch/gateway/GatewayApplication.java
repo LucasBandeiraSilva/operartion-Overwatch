bff1bc6b3b3f7e3d9d107ac26d9dc049ef046955
Lucas Bandeira <lucasbandeirai40@gmail.com>
2026-01-08 15:50:32 -0300

    feat(gateway): add Spring Cloud Gateway as system entry point

---
+++ gateway/src/main/java/com/overwatch/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.overwatch.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+    @Bean
+    public RouteLocator gatewayRouteLocator( RouteLocatorBuilder builder ){
+        return builder.routes()
+                .route(route->route.path("/agent/**").uri("lb://OW-agents"))
+                .route(route->route.path("/supers/**").uri("lb://OW-supers"))
+                .route(route->route.path("/bucket/**").uri("lb://OW-logistic"))
+                .build();
+    }
+
+}
